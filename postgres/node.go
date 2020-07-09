251451724524cdbca540b3fb56abe5b973caf0d3
Michito Maeda <michito.maeda@gmail.com>
2020-07-10 00:13:07 +0900

    moved the processing of `node` to single file.

--- postgres/node.go
+++ postgres/node.go
@@ -1,12 +1,16 @@
 package postgres
 
 import (
+	"fmt"
+
 	"database/sql"
 	"time"
 
+	"github.com/jmoiron/sqlx"
 	"micheam.com/ns"
 )
 
+// RowNode は、TODO
 type RowNode struct {
 	Id          string         `db:"id"`
 	Name        string         `db:"name"`
@@ -15,6 +19,7 @@ type RowNode struct {
 	UpdatedAt   time.Time      `db:"updated_at"`
 }
 
+// AsNode は、 TODO
 func (r *RowNode) AsNode() (*ns.Node, error) {
 	node := &ns.Node{
 		ID:   ns.NodeID(r.Id),
@@ -25,4 +30,28 @@ func (r *RowNode) AsNode() (*ns.Node, error) {
 		node.Description = &desc
 	}
 	return node, nil
+}
+
+// nodeReader は、 TODO
+type nodeReader struct {
+	db *sqlx.DB
+}
+
+func NewPostgresNodeReader() (ns.NodeReader, error) {
+	db, err := GetConn()
+	if err != nil {
+		return nil, fmt.Errorf("failed to init PostgresNodeReader: %w", err)
+	}
+	return &nodeReader{db: db}, nil
+}
+
+// GetByID は、Postgresql から node を抽出して返却する。
+//
+// TODO(micheam): interface sqlx.Queryer を使って sqlx.DB と sqlx.Tx を透過的に扱う
+func (p *nodeReader) GetByID(owner ns.User, id ns.NodeID) (*ns.Node, error) {
+	row := &RowNode{}
+	if err := p.db.Get(row, "SELECT * FROM node WHERE id = $1", id); err != nil {
+		return nil, fmt.Errorf("failed to get node(id %s): %w", id, err)
+	}
+	return row.AsNode()
 }
--- postgres/node_test.go
+++ postgres/node_test.go
@@ -5,7 +5,9 @@ import (
 	"testing"
 
 	"github.com/google/uuid"
+	_ "github.com/lib/pq"
 	"github.com/stretchr/testify/assert"
+	"micheam.com/ns"
 )
 
 func TestRowNode_AsNode_returns_a_node(t *testing.T) {
@@ -30,4 +32,42 @@ func TestRowNode_AsNode_returns_a_node(t *testing.T) {
 	assert.EqualValues(name, got.Name, "Nameが付与されていること")
 	assert.NotNil(got.Description, "DescriptionがNilでないこと")
 	assert.EqualValues(desc, *got.Description, "Descriptionがふよされていること")
+}
+
+func TestPostgresNodeReader_GetByID(t *testing.T) {
+	var (
+		assert  = assert.New(t)
+		db, err = GetConn()
+		owner   = ns.User{}
+		id      = uuid.New().String()
+	)
+	assert.NoError(err)
+	MustInsertNode(db, &RowNode{Id: id, Name: "test"})
+	t.Cleanup(func() { CleanupAll(db) })
+
+	sut, err := NewPostgresNodeReader()
+	assert.NoError(err)
+
+	got, gotErr := sut.GetByID(owner, ns.NodeID(id))
+	assert.NoError(gotErr)
+	assert.NotNil(got)
+	assert.EqualValues(got.ID, id, "指定されたIDのNODEが抽出される")
+}
+
+func TestPostgresNodeReader_GetByID_NoExist(t *testing.T) {
+	var (
+		assert  = assert.New(t)
+		db, err = GetConn()
+		owner   = ns.User{}
+		id      = uuid.New().String()
+	)
+	assert.NoError(err)
+	t.Cleanup(func() { CleanupAll(db) })
+
+	sut, err := NewPostgresNodeReader()
+	assert.NoError(err)
+
+	_, gotErr := sut.GetByID(owner, ns.NodeID(id))
+	t.Logf("%T", gotErr)
+	assert.Error(gotErr)
 }
